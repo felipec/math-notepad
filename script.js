c3eef3bb9a32eee9ce3b6de905f8ed6d9dbf792e
Felipe Contreras <felipe.contreras@gmail.com>
2021-12-23 19:09:45 -0600

    Add placeholder to actually do math
    After a certain timer has expired (100 ms for now).
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const wait = 100;
+
 var editor = ace.edit(input, {
   theme: 'ace/theme/monokai',
   printMargin: false,
@@ -7,3 +9,18 @@ var results = ace.edit(output, {
   printMargin: false,
   readOnly: true,
 });
+
+function doMath(input) {
+  console.debug('do math');
+}
+
+var timer;
+
+function onChange(change) {
+  clearTimeout(timer);
+  timer = setTimeout(doMath, wait, editor.getValue());
+}
+
+editor.on('change', onChange);
+
+doMath(editor.getValue());
