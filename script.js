3ff2dd52c8b8dca9e00848d3fee542e038fe9248
David <dvd.cnt@gmail.com>
2021-12-29 12:26:37 -0600

    Outputs include line numbers

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const wait = 100;
 
+const parser = self.math.parser()
 function doMath(input) {
-  let output = [];
-  let scope = {};
-
-  for (const line of input.split('\n')) {
-    let output_line = '';
-    if (line) {
-      if (line.startsWith('#')) {
-        output_line = line;
-      } else {
-        try {
-          output_line = math.evaluate(line, scope);
-        } catch(e) {
-          output_line = e;
-        }
+  if (input) {
+    let output = [];
+    const inputs = input.split('\n');
+    parser.clear();
+    inputs.forEach((input, I) => {
+      try {
+        output_line = parser.evaluate(input);
       }
-    }
-    output.push(output_line.toString());
+      catch (e) {
+        output_line = e;
+      }
+      if (output_line && output_line.toString() != "[]" && typeof (output_line) != "function" && output_line.toString() != "[object Object]") {
+        output.push((I + 1) + ":" + output_line.toString().split("\n").map(l => "\t" + l).join("\n"))
+      }
+    })
+    results.updateCode(output.join("\n"))
   }
-
-  results.updateCode(output.join('\n'));
 }
 
 var timer;
