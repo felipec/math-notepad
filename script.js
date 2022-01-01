56aa3eb2e817cdf2e379f84ae3a0ecec9ef47230
David <dvd.cnt@gmail.com>
2022-01-01 09:08:03 -0600

    Add files via upload
    Changed back to original form for consistency.
    Better evaluation for functions and objects.
    
    sin(30 deg)
    
    f(x,y) = x+y
    
    f(2,3)
    
    car = {mass: 10 kg, color: "blue"}

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const wait = 100;
 
-const parser = self.math.parser()
 function doMath(input) {
-  if (input) {
-    let output = [];
-    const inputs = input.split('\n');
-    parser.clear();
-    inputs.forEach((input, inputIndex) => {
-      try {
-        output_line = parser.evaluate(input);
+  let output = [];
+  let scope = {};
+
+  for (const line of input.split('\n')) {
+    let output_line = '';
+    if (line) {
+      if (line.startsWith('#')) {
+        output_line = line;
+      } else {
+        try {
+          output_line = math.format(math.evaluate(line, scope),14);
+        } catch(e) {
+          output_line = e;
+        }
       }
-      catch (e) {
-        output_line = e;
-      }
-      // Checks for unwanted outputs like [], function() ..., null, etc.
-      if (output_line && output_line.toString() != "[]" && typeof (output_line) != "function" && output_line.toString() != "[object Object]") {
-        // Formats the output to show from which line it comes from and accounts for multiple line outputs
-        output.push((inputIndex + 1) + ":" + math.format(output_line,14).split("\n").map(l => "\t" + l).join("\n"))
-      }
-    })
-    results.updateCode(output.join("\n"))
+    }
+    output.push(output_line.toString());
   }
+
+  results.updateCode(output.join('\n'));
 }
 
 var timer;
