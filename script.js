9bc4be87d66fd8d7adf3fb93a353c35d269b0a0d
Felipe Contreras <felipe.contreras@gmail.com>
2022-01-30 16:23:39 -0600

    Add help section
    Partially solves issue #1.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const wait = 100;
 
+function showDoc(doc) {
+  if (!doc) {
+    help.style.display = 'none';
+    return;
+  }
+
+  help_name.textContent = doc.name;
+  help_description.textContent = doc.description;
+  help_syntax.textContent = doc.syntax.join("\n");
+  hljs.highlightElement(help_syntax);
+  help_examples.textContent = doc.examples.join("\n");
+  hljs.highlightElement(help_examples);
+  help_seealso.textContent = doc.seealso.join(", ");
+  help.style.display = 'block';
+}
+
 function doMath(input) {
   let output = [];
   let scope = {};
+  let doc;
 
   for (const line of input.split('\n')) {
     let output_line = '';
@@ -11,7 +28,9 @@ function doMath(input) {
         output_line = line;
       } else {
         try {
-          output_line = math.format(math.evaluate(line, scope), 14);
+          const r = math.evaluate(line, scope);
+          if (r.doc) doc = r.doc;
+          else output_line = math.format(r, 14);
         } catch(e) {
           output_line = e.toString();
         }
@@ -21,6 +40,7 @@ function doMath(input) {
   }
 
   results.updateCode(output.join('\n'));
+  showDoc(doc);
 }
 
 var timer;
