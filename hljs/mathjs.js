f505bd0bfd15f4a7cf0a15fe4c1a739c2e621ac4
Felipe Contreras <felipe.contreras@gmail.com>
2023-08-23 02:11:23 -0600

    hljs: hardcode list of functions
    Not all functions of the math object are official functions of math.js.
    
    And we don't want to be running code to generate these lists every time.
    
    Fixes #4.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- hljs/mathjs.js
+++ hljs/mathjs.js
@@ -40,7 +40,28 @@ function mathjs(hljs) {
     begin: /[\[\],;]/,
   };
 
-  const FUNCTION_LIST = Object.getOwnPropertyNames(math).filter(e => typeof math[e] === 'function');
+  const FUNCTION_LIST = ['config', 'abs', 'acos', 'acosh', 'acot', 'acoth', 'acsc', 'acsch', 'add', 'addScalar', 'and',
+    'arg', 'asec', 'asech', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'bellNumbers', 'bignumber', 'bin', 'bitAnd',
+    'bitNot', 'bitOr', 'bitXor', 'boolean', 'catalan', 'cbrt', 'ceil', 'clone', 'column', 'combinations',
+    'combinationsWithRep', 'compare', 'compareNatural', 'compareText', 'compile', 'complex', 'composition', 'concat',
+    'conj', 'cos', 'cosh', 'cot', 'coth', 'count', 'createUnit', 'cross', 'csc', 'csch', 'ctranspose', 'cube', 'cumsum',
+    'deepEqual', 'derivative', 'det', 'diag', 'diff', 'distance', 'divide', 'divideScalar', 'dot', 'dotDivide',
+    'dotMultiply', 'dotPow', 'eigs', 'equal', 'equalScalar', 'equalText', 'erf', 'evaluate', 'exp', 'expm', 'expm1',
+    'factorial', 'fft', 'filter', 'fix', 'flatten', 'floor', 'forEach', 'format', 'fraction', 'gamma', 'gcd',
+    'getMatrixDataType', 'hasNumericValue', 'help', 'hex', 'hypot', 'identity', 'ifft', 'im', 'index', 'intersect',
+    'inv', 'invmod', 'isInteger', 'isNaN', 'isNegative', 'isNumeric', 'isPositive', 'isPrime', 'isZero', 'kldivergence',
+    'kron', 'larger', 'largerEq', 'lcm', 'leafCount', 'leftShift', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'lsolve',
+    'lsolveAll', 'lup', 'lusolve', 'mad', 'map', 'matrix', 'matrixFromColumns', 'matrixFromFunction', 'matrixFromRows',
+    'max', 'mean', 'median', 'min', 'mod', 'mode', 'multinomial', 'multiply', 'multiplyScalar', 'norm', 'nthRoot',
+    'nthRoots', 'number', 'numeric', 'oct', 'ones', 'or', 'parse', 'parser', 'partitionSelect', 'permutations',
+    'pickRandom', 'pinv', 'pow', 'print', 'prod', 'qr', 'quantileSeq', 'random', 'randomInt', 'range', 'rationalize',
+    're', 'replacer', 'reshape', 'resize', 'resolve', 'reviver', 'rightArithShift', 'rightLogShift', 'rotate',
+    'rotationMatrix', 'round', 'row', 'sec', 'sech', 'setCartesian', 'setDifference', 'setDistinct', 'setIntersect',
+    'setIsSubset', 'setMultiplicity', 'setPowerset', 'setSize', 'setSymDifference', 'setUnion', 'sign', 'simplify',
+    'simplifyConstant', 'simplifyCore', 'sin', 'sinh', 'size', 'slu', 'smaller', 'smallerEq', 'sort', 'sparse',
+    'splitUnit', 'sqrt', 'sqrtm', 'square', 'squeeze', 'std', 'stirlingS2', 'string', 'subset', 'subtract', 'sum',
+    'symbolicEqual', 'tan', 'tanh', 'to', 'trace', 'transpose', 'typeOf', 'typed', 'unaryMinus', 'unaryPlus', 'unequal',
+    'unit', 'usolve', 'usolveAll', 'variance', 'xgcd', 'xor', 'zeros'];
 
   const FUNCTIONS = {
     scope: 'title',
